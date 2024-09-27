9495ebcc28f794cb8f7c83d6d495d57bee226cba
Subham-u <subhammahapatra004@gmail.com>
2024-09-27 19:03:52 +0530

    me4

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -3,6 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { uploadOnCloudinary} from "./cloudinary";
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY as string;
+const CLIPDROP_API_KEY = process.env.CLIP_API as string;
 const cloudinary = require('cloudinary').v2
 
 
@@ -22,7 +23,7 @@ export async function generateImage(imageDescription : string,name :string) {
     const response = await fetch('https://clipdrop-api.co/text-to-image/v1', {
       method: 'POST',
       headers: {
-        'x-api-key': "15d2ae6daed9f80f9437e52d6b4fddfa11eba7c6a7890c7b9e07b73bdc13063570d78b5750b66be463f72ed317fb6cd5",
+        'x-api-key': `${CLIPDROP_API_KEY}`,
       },
       body: form,
     });
@@ -46,59 +47,3 @@ export async function generateImage(imageDescription : string,name :string) {
     throw error; 
 }
 }
-
-// export async function generateImagePrompt(name: string) {
-//   try {
-//     const response = await openai.createChatCompletion({
-//       model: "gpt-3.5-turbo",
-//       messages: [
-//         {
-//           role: "system",
-//           content:
-//             "You are an creative and helpful AI assistance capable of generating interesting thumbnail descriptions for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled",
-//         },
-//         {
-//           role: "user",
-//           content: `Please generate a thumbnail description for my notebook titles ${name}`,
-//         },
-//       ],
-//     });
-//     const data = await response.json();
-//     const image_description = data.choices[0].message.content;
-//     return image_description as string;
-//   } catch (error) {
-//     console.log(error);
-//     throw error;
-//   }
-// }
-
-// export async function generateImage(image_description: string) {
-//   try {
-//     const response = await openai.createImage({
-//       prompt: image_description,
-//       n: 1,
-//       size: "256x256",
-//     });
-//     const data = await response.json();
-//     const image_url = data.data[0].url;
-//     return image_url as string;
-//   } catch (error) {
-//     console.error(error);
-//   }
-
-//   return "https://t4.ftcdn.net/jpg/02/66/72/41/360_F_266724172_Iy8gdKgMa7XmrhYYxLCxyhx6J7070Pr8.jpg";
-// }
-
-// export async function generateImage(Image : string) {  
-//   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-//   const prompt = `One Google Images of ${Image}`;
-//   console.log(prompt);
-
-//   const image_url = await model.generateContent(prompt);
-//   console.log("image:", image_url.response.text());
-//   // return image_url as string;
-//   // return image_url.response.text() as string;
-// }
-
-// const fs = require('fs');
